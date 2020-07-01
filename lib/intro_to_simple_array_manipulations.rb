e6424593301ca4ab057e4d8cc07141d830640f28
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-01 13:01:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, string)
+array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end 
+def using_pop(array)
+element=array.pop 
+end
+def using_shift(array)
+  element=array.shift
+end
+def shift_with_args
+end
+  
